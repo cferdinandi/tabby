a3778eeca4e4af1386460e8b11f63c06a0d681d2
Chris Ferdinandi <hello@gomakethings.com>
2013-01-22 19:58:50 -0500

    Initial Release

---
+++ README.md
@@ -0,0 +1,12 @@
+# Tabby - Simple, mobile-first toggle tabs.
+Tabby is a lightweight JavaScript and CSS kit for mobile-first toggle tabs. It's style-light so you can easily modify it to fit your design.
+
+## How It Works
+Getting started with Tabby is really easy. [View the online tutorial](http://cferdinandi.github.com/tabby/) or dig through the example.html file.
+
+## Changelog
+* 01/22/2013
+  * Initial release.
+
+## License
+Tabby is licensed under the [WTFPL](http://www.wtfpl.net/) + "Not going to maintain this because the rent is too damn high" License.
---
+++ tabby.js
@@ -0,0 +1,34 @@
+/* =============================================================
+ * tabby.js v1.0
+ * Simple, mobile-first toggle tabs.
+ * Script by Chris Ferdinandi - http://gomakethings.com
+ * Licensed under WTFPL - http://www.wtfpl.net
+ * ============================================================= */
+
+$(function () {
+    $('.tabs a').click(function(e) {
+        e.preventDefault(); // Prevent default link behavior.
+        var tabID = $(this).attr('href'); // Pull the href value as the tabID.
+
+        $(this).addClass('active').parent().addClass('active'); // Add the .active class to the link and it's parent li (if one exists).
+        $(this).siblings().removeClass('active'); // Remove the .active class from sibling tab navigation elements.
+        $(this).parent('li').siblings().removeClass('active').children().removeClass('active'); // Remove the .active class from sibling li elements and their links.
+        $(tabID).addClass('active'); // Add the .active class to the div with the tab content.
+        $(tabID).siblings().removeClass('active'); // Remove the .active class from other tab content divs.
+    });
+});
+
+
+
+
+
+/* =============================================================
+ * accessibility-styles.js v1.0.0
+ * Adds .js class to <body> for progressive enhancement.
+ * Script by Chris Ferdinandi - http://gomakethings.com
+ * Licensed under WTFPL - http://www.wtfpl.net
+ * ============================================================= */
+
+$(function () {
+    $('body').addClass('js'); // On page load, add the .js class to the <body> element.
+});
