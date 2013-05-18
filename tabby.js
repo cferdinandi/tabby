521f55103cf3e59aae55fdc6ed86f3856a8691fd
Chris Ferdinandi <hello@gomakethings.com>
2013-05-17 22:43:36 -0400

    Set jQuery to noConflict mode for better compatability

--- tabby.js
+++ tabby.js
@@ -1,34 +1,38 @@
 /* =============================================================
- * tabby.js v1.0
+ * tabby.js
  * Simple, mobile-first toggle tabs.
  * Script by Chris Ferdinandi - http://gomakethings.com
  * Licensed under WTFPL - http://www.wtfpl.net
  * ============================================================= */
 
-$(function () {
-    $('.tabs a').click(function(e) {
-        e.preventDefault(); // Prevent default link behavior.
-        var tabID = $(this).attr('href'); // Pull the href value as the tabID.
+(function($) {
+    $(function () {
+        $('.tabs a').click(function(e) {
+            e.preventDefault(); // Prevent default link behavior.
+            var tabID = $(this).attr('href'); // Pull the href value as the tabID.
 
-        $(this).addClass('active').parent().addClass('active'); // Add the .active class to the link and it's parent li (if one exists).
-        $(this).siblings().removeClass('active'); // Remove the .active class from sibling tab navigation elements.
-        $(this).parent('li').siblings().removeClass('active').children().removeClass('active'); // Remove the .active class from sibling li elements and their links.
-        $(tabID).addClass('active'); // Add the .active class to the div with the tab content.
-        $(tabID).siblings().removeClass('active'); // Remove the .active class from other tab content divs.
+            $(this).addClass('active').parent().addClass('active'); // Add the .active class to the link and it's parent li (if one exists).
+            $(this).siblings().removeClass('active'); // Remove the .active class from sibling tab navigation elements.
+            $(this).parent('li').siblings().removeClass('active').children().removeClass('active'); // Remove the .active class from sibling li elements and their links.
+            $(tabID).addClass('active'); // Add the .active class to the div with the tab content.
+            $(tabID).siblings().removeClass('active'); // Remove the .active class from other tab content divs.
+        });
     });
-});
+})(jQuery);
 
 
 
 
 
 /* =============================================================
- * js-accessibility.js v1.0.0
+ * js-accessibility.js
  * Adds .js class to <body> for progressive enhancement.
  * Script by Chris Ferdinandi - http://gomakethings.com
  * Licensed under WTFPL - http://www.wtfpl.net
  * ============================================================= */
 
-$(function () {
-    $('body').addClass('js'); // On page load, add the .js class to the <body> element.
-});
+(function($) {
+    $(function () {
+        $('body').addClass('js'); // On page load, add the .js class to the <body> element.
+    });
+})(jQuery);
