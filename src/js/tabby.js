94425f000ae9d2be603531019b8c59d91a9f5969
cferdinandi <chris@gomakethings.com>
2016-08-30 17:04:08 -0400

    v9.4.3 Added escapeCharacters method to prevent querySelector breaking

--- src/js/tabby.js
+++ src/js/tabby.js
@@ -167,6 +167,86 @@
 	};
 
 	/**
+	 * Escape special characters for use with querySelector
+	 * @public
+	 * @param {String} id The anchor ID to escape
+	 * @author Mathias Bynens
+	 * @link https://github.com/mathiasbynens/CSS.escape
+	 */
+	var escapeCharacters = function ( id ) {
+
+			// Remove leading hash
+			if ( id.charAt(0) === '#' ) {
+				id = id.substr(1);
+			}
+
+			var string = String(id);
+			var length = string.length;
+			var index = -1;
+			var codeUnit;
+			var result = '';
+			var firstCodeUnit = string.charCodeAt(0);
+			while (++index < length) {
+				codeUnit = string.charCodeAt(index);
+				// Note: there’s no need to special-case astral symbols, surrogate
+				// pairs, or lone surrogates.
+
+				// If the character is NULL (U+0000), then throw an
+				// `InvalidCharacterError` exception and terminate these steps.
+				if (codeUnit === 0x0000) {
+					throw new InvalidCharacterError(
+						'Invalid character: the input contains U+0000.'
+					);
+				}
+
+				if (
+					// If the character is in the range [\1-\1F] (U+0001 to U+001F) or is
+					// U+007F, […]
+					(codeUnit >= 0x0001 && codeUnit <= 0x001F) || codeUnit == 0x007F ||
+					// If the character is the first character and is in the range [0-9]
+					// (U+0030 to U+0039), […]
+					(index === 0 && codeUnit >= 0x0030 && codeUnit <= 0x0039) ||
+					// If the character is the second character and is in the range [0-9]
+					// (U+0030 to U+0039) and the first character is a `-` (U+002D), […]
+					(
+						index === 1 &&
+						codeUnit >= 0x0030 && codeUnit <= 0x0039 &&
+						firstCodeUnit === 0x002D
+					)
+				) {
+					// http://dev.w3.org/csswg/cssom/#escape-a-character-as-code-point
+					result += '\\' + codeUnit.toString(16) + ' ';
+					continue;
+				}
+
+				// If the character is not handled by one of the above rules and is
+				// greater than or equal to U+0080, is `-` (U+002D) or `_` (U+005F), or
+				// is in one of the ranges [0-9] (U+0030 to U+0039), [A-Z] (U+0041 to
+				// U+005A), or [a-z] (U+0061 to U+007A), […]
+				if (
+					codeUnit >= 0x0080 ||
+					codeUnit === 0x002D ||
+					codeUnit === 0x005F ||
+					codeUnit >= 0x0030 && codeUnit <= 0x0039 ||
+					codeUnit >= 0x0041 && codeUnit <= 0x005A ||
+					codeUnit >= 0x0061 && codeUnit <= 0x007A
+				) {
+					// the character itself
+					result += string.charAt(index);
+					continue;
+				}
+
+				// Otherwise, the escaped character.
+				// http://dev.w3.org/csswg/cssom/#escape-a-character
+				result += '\\' + string.charAt(index);
+
+			}
+
+			return '#' + result;
+
+		};
+
+	/**
 	 * Stop YouTube, Vimeo, and HTML5 videos from playing when leaving the slide
 	 * @private
 	 * @param  {Element} content The content container the video is in
@@ -240,7 +320,7 @@
 	var toggleTabs = function ( tabID, settings ) {
 
 		// Variables
-		var tab = document.querySelector( tabID ); // The selected tab
+		var tab = document.querySelector( escapeCharacters( tabID ) ); // The selected tab
 		if ( !tab ) return;
 		var tabGroup = getClosest( tab, settings.selectorContentGroup ); // The parent for the tab group
 		if ( !tabGroup ) return;
@@ -268,7 +348,7 @@
 
 		// Selectors and variables
 		var toggleSettings = extend( settings || defaults, options || {} );  // Merge user options with defaults
-		var tabs = document.querySelectorAll(tabID); // Get tab content
+		var tabs = document.querySelectorAll( escapeCharacters( tabID ) ); // Get tab content
 
 		// Toggle visibility of the toggles and tabs
 		toggleToggles(toggle, toggleSettings);
